2a307ad6389a880e52b64e6ca2f61f40538b0a6e
gibo <gianni.boscarino@gmail.com>
2017-05-29 21:46:34 +0200

    changed

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,47 @@
 import { render } from 'react-dom';
 import React, { createElement as __, DOM as _ } from 'react';
-import { Mosaic, MosaicWindow } from 'react-mosaic-component';
+import { Mosaic, MosaicWindow, Expand, Remove, DEFAULT_CONTROLS_WITHOUT_CREATION } from 'react-mosaic-component';
 import '@blueprintjs/core/dist/blueprint.css';
 import "react-mosaic-component/react-mosaic-component.css";
 import './index.less';
 
+
+const sections = {
+
+    documents: {
+        title: "documents",
+        content: __('div', {className: 'content'}, ['left', _.br(), 'aaaa'] )
+    },
+
+    facets: {
+        title: "facets",
+        content: __('div', {className: 'content'}, 'right'),
+    },
+
+    preview: {
+        title: "preview",
+        content: __('div', {className: 'content'}, 'preview'),
+    },
+
+};
+
+
+
+
+
+
+
 function App1 () {
     return __(Mosaic, {
     
-        renderTile: e => __(MosaicWindow, {createNode: f => f, title: 'hello'}),
+        renderTile: e => __(MosaicWindow, {toolbarControls: DEFAULT_CONTROLS_WITHOUT_CREATION, title: sections[e].title}, sections[e].content),
         initialValue: {
-            direction: 'row',
-            first: __('div', {className: 'content'}, ['left', _.br(), 'aaaa'] ),
+            direction: 'column',
+            first: 'facets',
             second: {
-                direction: 'column',
-                first: __('div', {className: 'content'}, 'right'),
-                second: __('div', {className: 'content'}, 'right')
+                direction: 'row',
+                first: 'documents',
+                second: 'preview'
             }
         }
     }); 
